cf51863afe5ef317b9dc75422f5dd524e08061ff
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-09-18 15:50:36 -0700

    add all methods to styles controller

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -1,31 +1,51 @@
 class StylesController < ApplicationController
 
   def index
-
+    @styles = Style.all
+    render :index
   end
 
   def new
-
+    @style = Style.new
+    render :new
   end
 
   def create
-
+    @style = Style.new(style_params)
+    if @style.save 
+      redirect_to styles_path 
+    else
+      render :new
+    end 
   end
 
   def edit
-
+    @style = Style.find(params[:id])
+    render :edit
   end
 
   def show
-
+    @style = Style.find(params[:id])
+    render :show
   end
 
   def update
-
+    @style = Style.find(params[:id])
+    if @style.update(style_params)
+      redirect_to styles_path 
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @style = Style.find(params[:id])
+    @style.destroy
+    redirected_to styles_path
   end
 
+  private 
+    def style_params
+      params.require(:style).permit(:name)
+    end  
 end
