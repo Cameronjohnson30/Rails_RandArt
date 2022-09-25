3989b8d00fe46fc0b75ce9c2169f88e1cb8a1483
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-09-25 10:29:46 -0700

    run db migrate to update db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_25_171246) do
+ActiveRecord::Schema.define(version: 2022_09_25_172649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,10 +22,18 @@ ActiveRecord::Schema.define(version: 2022_09_25_171246) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "ideas", force: :cascade do |t|
+    t.string "name"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "styles", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "ideas", "categories"
 end
