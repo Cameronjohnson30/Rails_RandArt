c7ee7fe6aab9d148a0c7eaf3089253e110c3e6b7
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-09-29 23:07:20 -0700

    add devise for and resources, add resources categories do ideas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :user
+  resources :users
+  
   root to: 'styles#index'
+      resources :styles do 
+      resources :categories 
+    end
   resources :categories do 
     resources :ideas
   end
-    resources :styles  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
