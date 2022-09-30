877606a6ad5b470e48e29ede4a130116c89cb6bd
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-09-29 23:14:21 -0700

    change methods to include style for one to many relation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,42 +1,47 @@
 class CategoriesController < ApplicationController
-  
-  def index 
 
-    @categories = Category.all
+  def index
+    @category = Category.all
     render :index
   end
 
   def new
-    @category = Category.new
+    @style = Style.find(params[:style_id])
+    @category = @style.categories.new
     render :new
   end
 
   def create
-    @category = Category.new(category_params)
+    @style = Style.find(params[:style_id])
+    @category = @style.categories.new(category_params)
     if @category.save
       flash[:notice] = "Category successfully added!"
-      redirect_to categories_path
+      redirect_to style_path(@style)
     else
-      render :new
+      flash[:notice] = "error"
+      render :new, status: :unprocessable_entity 
     end
   end
 
-  def edit
-    @category = Category.find(params[:id])
-    render :edit
-  end
-
   def show
+    @style = Style.find(params[:style_id])
     @category = Category.find(params[:id])
     render :show
+  end
+
+  def edit
+    @style = Style.find(params[:style_id])
+    @category = Category.find(params[:id])
+    render :edit
   end
 
   def update
     @category = Category.find(params[:id])
     if @category.update(category_params)
       flash[:notice] = "Category successfully updated!"
-      redirect_to categories_path
+      redirect_to style_path(@category.style)
     else
+      @style = Style.find(params[:style_id])
       render :edit
     end
   end
@@ -45,12 +50,11 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @category.destroy
     flash[:notice] = "Category successfully DESTROYED!!"
-    redirect_to categories_path
+    redirect_to style_path(@category.style)
   end
 
   private 
     def category_params
       params.require(:category).permit(:name)
-  end
+    end  
 end
-
