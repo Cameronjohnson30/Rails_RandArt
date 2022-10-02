64648e5cbd379e4f16a6b33670427ad9504abe52
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-10-02 12:19:28 -0700

    add method to display random db entries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  helper_method :random
+  helper_method :rando
+
+def rando(category)
+  # random_style = Style.all.sample
+  # :style_id == random_style.id
+  # style = Style.find(params[:style_id])
+  # @category = Category.all.sample
+  # return category
+end
+  def random(style)
+    random_style = Style.all.sample
+    random = random_style.name
+    style_id = random_style.id
+    category = Category.all.sample
+    rano = category.name
+    random_idea = Idea.all.sample
+    random_id = random_idea.name
+    return "Style:" + " " + random + ", "+ "Category:" + " " + rano + ", " + "Idea:" + random_id
+  end
 end
