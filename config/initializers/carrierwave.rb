e200bac0194655f97b67d1b9fe5c70705c06967b
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-10-02 16:50:04 -0700

    test to see if hidden

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     provider:              'AWS',                            # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],            # required
+    aws_access_key_id:     ],            # required
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],            # required
     region:                'us-east-1'                       # to match the carrierwave and bucket region
   }
