0cd03f0af7aaf0c2308a8325a2037c36a67fd64d
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-10-03 02:19:54 -0700

    add is admin, and params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :random
   helper_method :rando
-
+  def is_admin?
+    current_user && current_user.admin
+  end
 def rando(category)
   # random_style = Style.all.sample
   # :style_id == random_style.id
@@ -18,5 +21,11 @@ end
     random_idea = Idea.all.sample
     random_id = random_idea.name
     return "Style:" + " " + random + ", "+ "Category:" + " " + rano + ", " + "Idea:" + random_id
+  end
+
+  protected
+  def configure_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :firstname, :lastname, :email, :password, :image_cache])
+    devise_parameter_sanitizer.permit(:update_user, keys: [:username, :firstname, :lastname, :email, :image_cache])
   end
 end
