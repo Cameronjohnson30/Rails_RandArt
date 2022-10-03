cf3b58bf49b26b76faf71de4b16c52d888d3968a
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-10-03 02:26:07 -0700

    change show to new, update create method, add show, params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,26 @@
 class PostsController < ApplicationController
 
-  def show
-    Apost = Post.new
+  def new
+    @post = Post.new
   end
 
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id if user_signed_in?
+    if @post.save
+      redirect_to :root_path,
+      flash[:notice] = "Post successfully added!"
+    else
+      redirect_to new_post_path,
+      flash[:notice] = "error"
+    end
   end
-  private post_params
-  params.reequire(:post).permit(:image)
-end
+
+  def show
+  end
+
+  private 
+  def post_params
+  params.require(:post).permit(:image, :image_cache)
+  end
 end
