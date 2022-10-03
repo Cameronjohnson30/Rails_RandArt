5110893c5b8bb42233888394d432c41cb7b523d1
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-10-03 02:22:03 -0700

    change to has one attached photo, belongs to user, before create, scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
-  mount_uploader :image, ImageUploader
+  has_one_attached :image
+  belongs_to :user
+  before_create :set_active
+  scope :active, -> { where active: true }
+
 end
