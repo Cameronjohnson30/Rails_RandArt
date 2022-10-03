a7f149890798da2d73475858406f3f07154bd55e
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-10-03 02:14:59 -0700

    add functions total posts, followers, following set to 0

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts
+  def total_posts
+  0
+  end
+  
+  def total_followers
+  0
+  end
+
+  def total_following
+  0
+  end
 end
