999b2788adad819e2bcd667bcbed3533db27d5e4
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-10-05 03:06:45 -0700

    delete rando method, and image_cache

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :random
-  helper_method :rando
   def is_admin?
     current_user && current_user.admin
   end
-def rando(category)
-  # random_style = Style.all.sample
-  # :style_id == random_style.id
-  # style = Style.find(params[:style_id])
-  # @category = Category.all.sample
-  # return category
-end
+
   def random(style)
+    @styles = Style.all
     random_style = Style.all.sample
     random = random_style.name
     style_id = random_style.id
+    @categories = Category.all
     category = Category.all.sample
     rano = category.name
+    @ideas = Idea.all
     random_idea = Idea.all.sample
     random_id = random_idea.name
     return "Style:" + " " + random + ", "+ "Category:" + " " + rano + ", " + "Idea:" + random_id
@@ -25,7 +21,7 @@ end
 
   protected
   def configure_permitted_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :firstname, :lastname, :email, :password, :image_cache])
-    devise_parameter_sanitizer.permit(:update_user, keys: [:username, :firstname, :lastname, :email, :image_cache])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :firstname, :lastname, :email, :password])
+    devise_parameter_sanitizer.permit(:update_user, keys: [:username, :firstname, :lastname, :email])
   end
 end
