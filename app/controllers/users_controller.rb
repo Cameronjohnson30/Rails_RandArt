1240b00060a6d0a20c05c2471356e91ee0d7170c
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-10-05 03:22:29 -0700

    add before action, change index, add set user, change update to render error, add destroy, delete admin from params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:profile]
 
   def index
-    @users = User.all
+    @posts = Post.active
   end
 
   def show 
@@ -12,18 +13,34 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+
+
+  def set_user
+  @user = User.find_by_username(params[:username])
+  end
+
   def update
     respond_to do |format|
       if current_user.update(user_params)
         format.html { redirect_to current_user, notice: "You successfully updated your profile." }
       else
-        format.html { render :edit }
+        format.html { render :edit, notice: "error" }
       end
     end
   end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "Idea was deleted"
+    render :index
+  end
+end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:username, :email)
+    end
 
-  private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :admin)
+    params.require(:user).permit(:email, :password, :password_confirmation, :username, :first_name, :last_name)
   end
 end
