bf25e05a59f156496b88526f1c3f43b252fd322d
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-10-05 03:08:56 -0700

    add index change create renders, and add show, params description

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,23 +4,38 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def index
+    @post = Post.all 
+    render :index
+  end
+
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id if user_signed_in?
     if @post.save
-      redirect_to :root_path,
+      render :show
       flash[:notice] = "Post successfully added!"
     else
-      redirect_to new_post_path,
+      redirect_to new_post_path
       flash[:notice] = "error"
     end
   end
 
   def show
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+    render :show
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "Idea was deleted"
+    render :index
   end
 
   private 
   def post_params
-  params.require(:post).permit(:image, :image_cache)
+  params.require(:post).permit(:image, :description)
   end
+
 end
