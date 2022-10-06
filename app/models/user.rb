883a4a88cabcb35c89ba2263991628a8a73e981b
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-10-06 05:52:52 -0700

    add has many likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :likes
   has_many :posts
   def total_posts
   0
