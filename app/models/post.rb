b044174f47d570e2c670f2d00eebd2bc7adb7a9e
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-10-06 05:38:25 -0700

    add has many likes and comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   default_scope { order created_at: :desc }
   has_one_attached :image
   belongs_to :user
+  has_many :likes
+  has_many :comments
   scope :active, -> { where active: true }
   
   def total_likes
