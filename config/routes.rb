c50b79a52b6747868e04cf1b5d23e4126c8a4e8b
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-10-06 05:58:23 -0700

    add get and post routes, resources comments, add index to posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :users
   
   get "profile/:username" => "users#profile", as: :profile
+  get "post/like/:post_id" => "likes#save_like", as: :like_post
+  post "follow/user" => "user#follow_user", as: :follow_user
   root to: 'styles#index'
       resources :styles do 
       resources :categories 
@@ -10,6 +12,7 @@ Rails.application.routes.draw do
   resources :categories do 
     resources :ideas
   end
-resources :posts, only: [:new,:create,:show]
+resources :posts, only: [:new,:create,:show, :index]
+resources :comments, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
