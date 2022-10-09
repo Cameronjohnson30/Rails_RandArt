1904c5b32da0b004e626ed0e1c5926b78839909d
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-10-09 00:21:27 -0700

    WIP still doesnt work

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,15 @@
 class LikesController < ActionController
+before_action :authenticate_user!
 
   def save_like
-  @like = linke.new(post_id: params[:post_id], user_id:, current_user.id)
+  @like = Like.new(post_id: params[:post_id], user_id:, current_user.id)
   @post_id = params[:post_id]
-    existing_like = Like.where(post_id: params[:post_id], user_id: current_user.id)
-  reapond_to do |format|
+
+
+    respond_to do |format|
       format.js {
-        if existing_like.any?
-          existing_like.first.destroy
-          @success = false
-        elsif @like.save
+        if @like.save
         @success: true 
-        render "posts/like"
       else 
         @success: false 
       end
