f7d725f3aca4ab9fb2758c2f739e916918ecb9eb
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-10-08 23:59:10 -0700

    add registraion controller to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :user
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users
-  
   get "profile/:username" => "users#profile", as: :profile
   get "post/like/:post_id" => "likes#save_like", as: :like_post
   post "follow/user" => "user#follow_user", as: :follow_user
+
   root to: 'styles#index'
-      resources :styles do 
-      resources :categories 
-    end
-  resources :categories do 
+    resources :styles do 
+    resources :categories 
+  end
+    resources :categories do 
     resources :ideas
   end
-resources :posts, only: [:new,:create,:show, :index]
-resources :comments, only: [:create, :destroy]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :posts, only: [:new,:create,:show, :index]
+    resources :comments, only: [:create, :destroy]
 end
