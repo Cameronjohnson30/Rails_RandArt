f6e49ce1a737cb3f757e4687e7dab78a1f598781
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-10-09 00:23:41 -0700

    fix spacing, random method now works as intended, spelling corrections in params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,26 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :random
+
   def is_admin?
     current_user && current_user.admin
   end
 
+
   def random(style)
-    @styles = Style.all
     random_style = Style.all.sample
     random = random_style.name
     style_id = random_style.id
-    @categories = Category.all
-    category = Category.all.sample
-    rano = category.name
-    @ideas = Idea.all
-    random_idea = Idea.all.sample
+    random_category = Category.find_by_style_id(style_id)
+    rano = random_category.name
+    category_id = random_category.id
+    random_idea = Idea.find_by_category_id(category_id)
     random_id = random_idea.name
     return "Style:" + " " + random + ", "+ "Category:" + " " + rano + ", " + "Idea:" + random_id
   end
-
-  protected
+protected
   def configure_permitted_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :firstname, :lastname, :email, :password])
-    devise_parameter_sanitizer.permit(:update_user, keys: [:username, :firstname, :lastname, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :email, :password])
+    devise_parameter_sanitizer.permit(:update_user, keys: [:username, :first_name, :last_name, :email])
   end
 end
